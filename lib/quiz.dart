02cfe1bfa737e1afaf483af8cfcf92031c506601
Keelner1 <michal.cios777@gmail.com>
2025-01-22 22:08:49 +0100

    summary

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,6 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/results_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/start_screen.dart';
 
@@ -11,19 +13,29 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
   String activeScreen = 'start-screen';
   void switchScreen() {
     setState(() {
       activeScreen = 'questions-screen';
     });
   }
-
+  void chooseAnswer(String answer){
+    selectedAnswers.add(answer);
+    if(selectedAnswers.length == questions.length){
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
     Widget screenWidget = StartScreen(switchScreen);
 
     if(activeScreen == 'questions-screen'){
-      screenWidget = const QuestionsScreen();
+      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
+    }else if(activeScreen == 'results-screen'){
+      screenWidget = ResultsScreen(chosenAnswers: selectedAnswers,);
     }
 
     return MaterialApp(
---
+++ lib/results_screen.dart
@@ -0,0 +1,38 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:flutter/material.dart';
+
+class ResultsScreen extends StatelessWidget{
+  const ResultsScreen({super.key, required this.chosenAnswers});
+  final List<String> chosenAnswers;
+
+  List<Map<String,Object>>getSummaryData(){
+    final List<Map<String,Object>> summary = [];
+    for (int i=0;i<chosenAnswers.length;i++) {
+      summary.add({
+        'quesiton_index':i,
+        'question': questions[i],
+        'correct_answer':questions[i].answers[0],
+        'user_answer':chosenAnswers[i]
+        });
+    }
+    return summary;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('You answered x out of y'),
+            const SizedBox(height: 30,),
+            const Text('ListofQuestions'),
+            TextButton(onPressed: (){}, child: const Text('RestartQuiz'))
+          ],
+        ),
+      ),
+    );
+  }
+}

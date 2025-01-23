63be78e7c7d240b799a7420ea521cb9169f2faac
Keelner1 <michal.cios888@gmail.com>
2025-01-23 22:34:42 +0100

    almostend

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,4 +1,5 @@
 import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_summary.dart';
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget{
@@ -9,8 +10,8 @@ class ResultsScreen extends StatelessWidget{
     final List<Map<String,Object>> summary = [];
     for (int i=0;i<chosenAnswers.length;i++) {
       summary.add({
-        'quesiton_index':i,
-        'question': questions[i],
+        'question_index':i,
+        'question': questions[i].text,
         'correct_answer':questions[i].answers[0],
         'user_answer':chosenAnswers[i]
         });
@@ -28,7 +29,7 @@ class ResultsScreen extends StatelessWidget{
           children: [
             const Text('You answered x out of y'),
             const SizedBox(height: 30,),
-            const Text('ListofQuestions'),
+            QuestionsSummary(getSummaryData()),
             TextButton(onPressed: (){}, child: const Text('RestartQuiz'))
           ],
         ),
